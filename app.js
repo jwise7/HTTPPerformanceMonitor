ac7184863f5315d5390b2bed4f482d5423c55737
jwise7 <jwise7@gmail.com>
2016-06-07 11:16:22 -0500

    re-directed

--- app.js
+++ app.js
@@ -1,14 +1,92 @@
-var perfMon = require('./performanceMonitor');
-var argv = require('yargs').argv;
+//var perfMon = require('./performanceMonitor'),
+var	http = require('http'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express();
 
-if(typeof argv._ === "undefined" || typeof argv._[0] === "undefined" || argv._[0] == ""){
-	console.error("please provide a url");
-	process.exit();
+app.set('port', process.env.PORT || 4005);
+
+app.use(bodyParser.json({strict:false}))
+app.use(function(req,res,next){ //NOTE: allowing cors.
+	var origin = req.headers['origin'];
+	var originWhitelist = ['http://aclj.org', 'https://aclj.org', 'http://localhost:4000'];
+	if(originWhitelist.indexOf(origin)!=-1){
+		res.setHeader('Access-Control-Allow-Origin', origin);
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	}
+	
+	next();
+})
+app.post('/timings',function(req,res){
+	var timings=[];
+	if(typeof req.body !== "undefined"){
+		timings = cleansePerformanceData(req.body);
+	}
+	else{
+		return res.status(400).end();
+	}
+	if(timings.length>0){
+		var perfData={
+			userAgent:req.headers['user-agent'],
+			timings:timings
+		};
+		return dbClient.collection('performanceTimings').save(perfData,function(err,record){
+			debugger;
+			if(err==null){
+				return res.status(200).end();
+			}
+			return res.status(400).end();
+		});
+	}
+	else{
+		return res.status(400).end();
+	}
+});
+app.get('/benchmark',function(req,res){
+	//TODO: implement this.
+	//perfMon.pageBenchmark(url,count,pause);
+	res.status(404).end();
+});
+
+function cleansePerformanceData(data){
+	debugger;
+	//var whitelist=['connectEnd','connectStart','domainLookupEnd','domainLookupStart','duration','fetchStart','name','redirectEnd','redirectStart','requestStart','requestEnd','responseEnd','responseStart','secureConnectionStart','startTime'];
+	var whitelist=['duration','name']; //NOTE: trying to save db space.
+	if(!Array.isArray(data)) return []; //NOTE: checking for array before continuing.
+	var cleanedTimings=[];
+	for(var i=0;i<data.length;i++){
+		var timing=data[i];
+		var cleanedTiming={};
+		for(var j=0;j<whitelist.length;j++){
+			if(typeof timing[whitelist[j]] === 'string' || (!isNaN(timing[whitelist[j]]) && timing[whitelist[j]] != 0)){ //NOTE: checking for numeric before adding.
+				cleanedTiming[whitelist[j]]=timing[whitelist[j]];
+			}
+		}
+		if(Object.keys(cleanedTiming).length>0){ //NOTE: checking for the existence of fields before adding.
+			cleanedTimings.push(cleanedTiming);
+		}
+	}
+	return cleanedTimings;
 }
-if(typeof argv.count === "undefined"){
-	argv.count = 10;
-}
-if(typeof argv.pause === "undefined"){
-	argv.pause = 1000;
-}
-perfMon.pageBenchmark(argv._[0],argv.count,argv.pause);
+
+//Note: Setup db connection
+var mongoClient = require('mongodb').MongoClient;
+
+//WARNING: global variable!!!!
+//NOTE: it looks like people either remove async or use a global variable... I (Jesse) prefer keeping the async and I (David) concur
+dbClient=null;
+var password=process.env.password;
+mongoClient.connect("mongodb://badf0rm:" + password + "@ds015713.mlab.com:15713/badf0rm",{
+		db:{w:-1},
+		server:{socketOptions:{keepAlive: 100}}
+	},function(err, db){
+		if(err) { 
+			console.log(err); //handling errors elsewhere
+			//return console.dir(err);
+		}
+		dbClient = db;
+		http.createServer(app).listen(app.get('port'),function(){
+			console.log('Server running on port ' + app.get('port'));
+		});
+});
--- performanceMonitor.js
+++ performanceMonitor.js
@@ -1,94 +1,94 @@
-var phantom = require('phantom');
-var driver=require('node-phantom-simple');
+// var phantom = require('phantom');
+// var driver=require('node-phantom-simple');
 
-function benchmarkWithPhantom(url,cb){
-    var _ph, _page, _resources={};
+// function benchmarkWithPhantom(url,cb){
+//     var _ph, _page, _resources={};
 
-    phantom.create().then(ph => {
-      _ph = ph;
-      return _ph.createPage();
-    }).then(page => {
-      _page = page;
+//     phantom.create().then(ph => {
+//       _ph = ph;
+//       return _ph.createPage();
+//     }).then(page => {
+//       _page = page;
       
-      page.on('onResourceRequested', function(requestData, networkRequest) {
-        var urlKey = requestData.url.split('?')[0];
-        if(typeof _resources[urlKey] === 'undefined') _resources[urlKey] = {};
-        _resources[urlKey].start=new Date();
-      });
-      page.on('onResourceReceived', function(responseData) {
-        var urlKey = responseData.url.split('?')[0];
-        if(typeof _resources[urlKey] === 'undefined') _resources[urlKey] = {};
-        if(responseData.stage=='start'){
-            _resources[urlKey].startReply=new Date();
-        }else if(responseData.stage=='end'){
-            _resources[urlKey].endReply=new Date();
-        }
-      });
-      return _page.open(url);
-    }).then(status => {
-        return _resources;
-    }).then(resources => {
-        for(var i in resources){
-            resources[i].waiting=resources[i].startReply-resources[i].start;
-            resources[i].receiving=resources[i].endReply-resources[i].startReply;
-        }
-      cb(resources);
-      _page.close();
-      _ph.exit();
-    });
-}
-function benchmarkWithSlimer(url,cb){
-    driver.create({path:'./slimerjs-0.10.0/slimerjs'},function(err,sl) {
-        return sl.createPage(function(err,page) {
-            return page.open(url, function(err,status) {
-                return page.evaluate(
-                    function() { var timings = performance.getEntries();timings.unshift(performance.timing);return timings; },
-                    function(err,result){
-                        debugger;
-                        sl.exit();
-                        return cb(result);
-                });
-            });
-        });
-    });
-}
-exports.pageBenchmark = function(url,count,msPause){
-    _statsBenchmarks(url,count,msPause,[]);
-}
-function _statsBenchmarks(url,count,msPause,stats){
-    if(count==0){
-        return _outputStats(stats,'slimer');
-    }else{
-        return benchmarkWithSlimer(url,function(resources){
-            stats.push(resources);
-            setTimeout(function(url,count,msPause,stats){ return function(){ return _statsBenchmarks(url,count,msPause,stats)}}(url,count-1,msPause,stats),msPause);
-        })
-    }
-}
-function _outputStats(stats,type){
-    var finalStats = {};
-    if(type=='phantom'){
-        for(var i=0;i<stats.length;i++){
-            for(var j in stats[i]){
-                if(typeof finalStats[j] == "undefined"){
-                    finalStats[j] = {waitings:[],receivings:[],timings:[]};
-                }
-                finalStats[j].waitings.push(stats[i][j].waiting);
-                finalStats[j].receivings.push(stats[i][j].receiving);
-                finalStats[j].timings.push(stats[i][j].timings);
-            }
-        }
-        for(var k in finalStats){
-            finalStats[k].averageWaiting = _averageArray(finalStats[k].waitings);
-            finalStats[k].averageReceiving = _averageArray(finalStats[k].receivings);
-            finalStats[k].averageTotal = finalStats[k].averageWaiting+finalStats[k].averageReceiving;
-        }
-    }else if(type=='slimer'){
-        finalStats=stats;
-    }
-    console.log(finalStats);
-    setTimeout(process.exit,1000);
-}
-function _averageArray(arr){
-    return arr.reduce(function(p,c){ return p + c; }) / arr.length;
-}
+//       page.on('onResourceRequested', function(requestData, networkRequest) {
+//         var urlKey = requestData.url.split('?')[0];
+//         if(typeof _resources[urlKey] === 'undefined') _resources[urlKey] = {};
+//         _resources[urlKey].start=new Date();
+//       });
+//       page.on('onResourceReceived', function(responseData) {
+//         var urlKey = responseData.url.split('?')[0];
+//         if(typeof _resources[urlKey] === 'undefined') _resources[urlKey] = {};
+//         if(responseData.stage=='start'){
+//             _resources[urlKey].startReply=new Date();
+//         }else if(responseData.stage=='end'){
+//             _resources[urlKey].endReply=new Date();
+//         }
+//       });
+//       return _page.open(url);
+//     }).then(status => {
+//         return _resources;
+//     }).then(resources => {
+//         for(var i in resources){
+//             resources[i].waiting=resources[i].startReply-resources[i].start;
+//             resources[i].receiving=resources[i].endReply-resources[i].startReply;
+//         }
+//       cb(resources);
+//       _page.close();
+//       _ph.exit();
+//     });
+// }
+// function benchmarkWithSlimer(url,cb){
+//     driver.create({path:'./slimerjs-0.10.0/slimerjs'},function(err,sl) {
+//         return sl.createPage(function(err,page) {
+//             return page.open(url, function(err,status) {
+//                 return page.evaluate(
+//                     function() { var timings = performance.getEntries();timings.unshift(performance.timing);return timings; },
+//                     function(err,result){
+//                         debugger;
+//                         sl.exit();
+//                         return cb(result);
+//                 });
+//             });
+//         });
+//     });
+// }
+// exports.pageBenchmark = function(url,count,msPause){
+//     _statsBenchmarks(url,count,msPause,[]);
+// }
+// function _statsBenchmarks(url,count,msPause,stats){
+//     if(count==0){
+//         return _outputStats(stats,'slimer');
+//     }else{
+//         return benchmarkWithSlimer(url,function(resources){
+//             stats.push(resources);
+//             setTimeout(function(url,count,msPause,stats){ return function(){ return _statsBenchmarks(url,count,msPause,stats)}}(url,count-1,msPause,stats),msPause);
+//         })
+//     }
+// }
+// function _outputStats(stats,type){
+//     var finalStats = {};
+//     if(type=='phantom'){
+//         for(var i=0;i<stats.length;i++){
+//             for(var j in stats[i]){
+//                 if(typeof finalStats[j] == "undefined"){
+//                     finalStats[j] = {waitings:[],receivings:[],timings:[]};
+//                 }
+//                 finalStats[j].waitings.push(stats[i][j].waiting);
+//                 finalStats[j].receivings.push(stats[i][j].receiving);
+//                 finalStats[j].timings.push(stats[i][j].timings);
+//             }
+//         }
+//         for(var k in finalStats){
+//             finalStats[k].averageWaiting = _averageArray(finalStats[k].waitings);
+//             finalStats[k].averageReceiving = _averageArray(finalStats[k].receivings);
+//             finalStats[k].averageTotal = finalStats[k].averageWaiting+finalStats[k].averageReceiving;
+//         }
+//     }else if(type=='slimer'){
+//         finalStats=stats;
+//     }
+//     console.log(finalStats);
+//     setTimeout(process.exit,1000);
+// }
+// function _averageArray(arr){
+//     return arr.reduce(function(p,c){ return p + c; }) / arr.length;
+// }
